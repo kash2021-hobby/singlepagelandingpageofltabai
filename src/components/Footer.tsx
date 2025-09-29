a9e5ac4697181cc49badd260d640899a1cd96fd1
kash2021-hobby <kashyapnandan2021@gmail.com>
2025-09-29 16:55:32 +0530

    Update phone number in Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -27,7 +27,7 @@ export default function Footer() {
             <div className="space-y-3">
               <div className="flex items-center space-x-3">
                 <Phone className="w-5 h-5 text-blue-400" />
-                <span className="text-gray-300">+91 9164060961</span>
+                <span className="text-gray-300">+91 6000683808</span>
               </div>
               <div className="flex items-center space-x-3">
                 <Mail className="w-5 h-5 text-blue-400" />
